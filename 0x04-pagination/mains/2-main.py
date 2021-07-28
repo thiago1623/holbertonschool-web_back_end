e7cdee6de18494ba80046d3eb987c601d2b55e34
thiago1623 <thiago.96t16@gmail.com>
2021-07-28 14:12:19 -0500

    [UPDATE]:--add new changes

---
+++ 0x04-pagination/mains/2-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import sys
+
+
+if __name__ == '__main__':
+    sys.path.append('..')
+
+    Server = __import__('2-hypermedia_pagination').Server
+
+    server = Server()
+
+    print(server.get_hyper(1, 2))
+    print("---")
+    print(server.get_hyper(2, 2))
+    print("---")
+    print(server.get_hyper(100, 3))
+    print("---")
+    print(server.get_hyper(3000, 100))
